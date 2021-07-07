59177f5b652cd1df8a179e6e9e726be058763931
eternal7s <eternal7s@naver.com>
2021-07-08 00:02:39 +0900

    20210707

--- chapter12/src/test/writeFile-test.ts
+++ chapter12/src/test/writeFile-test.ts
@@ -3,12 +3,12 @@ import {writeFile} from "../fileApi/writeFile";
 import {mkdir} from "../fileApi/mkdir";
 import mkdirp = require('mkdirp')
 
-const writeTest = async(filename: string, data: any) => {
+const writeTest = async (filename: string, data: any) => {
     const result = await writeFile(filename, data)
     console.log(`write ${result} to ${filename}`)
 }
 
 mkdir('./data')
-.then(s => writeTest('./data/hello.txt', 'hello world'))
-.then(s => writeTest('./data/test.json', JSON.stringify({name: 'Jack', age: 32}, null, 2)))
-.catch((e: Error) => console.log(e.message))
+    .then(s => writeTest('./data/hello.txt', ' hello world'))
+    .then(s => writeTest('./data/test.json', JSON.stringify({name: 'Jack', age: 32}, null, 2)))
+    .catch((e: Error) => console.log(e.message))
